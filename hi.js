c36d5f258e8aa8726f6966e9cd0431c55702de46
lianecagara <nealiana.kaye.cagara@gmail.com>
2025-04-02 19:52:56 +0800

    feat(pinger): add Pinger class for performance tracking and update version to 3.0.38-PRERELEASE

--- hi.js
+++ hi.js
@@ -1,3 +1,8 @@
+// @ts-check
+import { Pinger } from "@cass-modules/pinger";
+import { defineEntry } from "@cass/define";
+import { SpectralCMDHome } from "@cassidy/spectral-home";
+
 export const meta = {
   name: "hi",
   author: "Liane Cagara",
@@ -11,34 +16,82 @@ export const meta = {
   noPrefix: false,
   whiteList: null,
   ext_plugins: {
-    "requester": "^1.2.0"
-  }
+    requester: "^1.2.0",
+  },
 };
 
-export const entry = {
+/**
+ * @type {Record<string, CommandEntry>}
+ */
+const entryConf = {
   async test({ input, output, commandName }) {
-  if (input.arguments[0] === "Error" && input.arguments[1]) {
-    throw new Error(input.arguments.slice(1).join(" "));
-  }
-  const messageInfo = await output.reply("Hello wazzup, you said " + (input.arguments.join(" ") || "none.") + "\nYour sender ID is " + input.senderID + " and thread id is: " + input.threadID);
-  
-  output.reaction("💜");
-  input.setReply(messageInfo.messageID, {
-    key: commandName
-  });
-},
+    if (input.arguments[0] === "Error" && input.arguments[1]) {
+      throw new Error(input.arguments.slice(1).join(" "));
+    }
+    const messageInfo = await output.reply(
+      "Hello wazzup, you said " +
+        (input.arguments.join(" ") || "none.") +
+        "\nYour sender ID is " +
+        input.senderID +
+        " and thread id is: " +
+        input.threadID
+    );
+
+    output.reaction("💜");
+    input.setReply(messageInfo.messageID, {
+      key: commandName,
+    });
+  },
   async debug({ output, input }) {
     output.reply(JSON.stringify(input, null, 2));
-  }
-}
+  },
+  async dbtest({ money, threadsDB, output }) {
+    /**
+     * @type {[string, Partial<UserData>]}
+     */
+    const payload = [
+      "test",
+      {
+        name: "Test",
+        money: 69,
+      },
+    ];
+    const pinger1 = new Pinger();
+    pinger1.recordPing();
+    await money.set(...payload);
+    const ping1 = pinger1.getLastPing();
 
+    const pinger2 = new Pinger();
+    pinger2.recordPing();
+    await money.setItem(...payload);
+    const ping2 = pinger2.getLastPing();
+
+    return output.reply(`money.set: ${ping1}\nmoney.setItem: ${ping2}`);
+  },
+  async dbtest2({ money, output, input }) {
+    const items = await money.getItem(input.sid);
+
+    items.inventory.at(0);
+  },
+};
+
+const home = new SpectralCMDHome({
+  entryConfig: entryConf,
+});
+
+export const entry = defineEntry((ctx) => home.runInContext(ctx));
+
+/**
+ *
+ * @param {CommandContext & { detectID: string }} param0
+ */
 export async function reply({ output, repObj, input, detectID, commandName }) {
-  
   input.delReply(detectID);
-  const messageInfo = await output.reply(`Why the hell are you replying to me?`);
+  const messageInfo = await output.reply(
+    `Why the hell are you replying to me?`
+  );
   output.reaction("😭");
   input.setReply(messageInfo.messageID, {
-    key: commandName
+    key: commandName,
   });
 }
-
