5de440993825d96b99921fd491405e3e439a8421
lianecagara <nealiana.kaye.cagara@gmail.com>
2025-04-04 09:39:07 +0800

    feat(defineEntry): enhance command entry definitions with overloaded signatures and add defineHome function

--- CommandFiles/commands/uid.js
+++ CommandFiles/commands/uid.js
@@ -24,3 +24,4 @@ export const entry = defineEntry(async ({ input, output }) => {
     ),
   });
 });
+
--- handlers/definers/defineEntry.ts
+++ handlers/definers/defineEntry.ts
@@ -4,6 +4,72 @@
  * @param entry - The command entry to be defined.
  * @returns The defined command entry.
  */
-export function defineEntry(entry: CommandEntry) {
+export function defineEntry(entry: CommandEntry): CommandEntry;
+
+/**
+ * Defines and returns a command entry.
+ *
+ * @param entry - The command entry to be defined.
+ * @returns The defined command entry.
+ */
+export function defineEntry(
+  entry: Record<string, CommandEntry>
+): Record<string, CommandEntry>;
+
+export function defineEntry(
+  entry: CommandEntry | Record<string, CommandEntry>
+) {
   return entry;
+}
+
+import { ReduxCMDHome } from "@cass-modules/reduxCMDHomeV2";
+import { SpectralCMDHome } from "@cassidy/spectral-home";
+
+/**
+ * Defines a home command entry for the SpectralCMD system.
+ *
+ * @param options - The primary configuration options for the `SpectralCMDHome` instance.
+ * These options are passed as the first parameter to the `SpectralCMDHome` constructor.
+ *
+ * @param config - Additional configuration for the `SpectralCMDHome` instance.
+ * This parameter corresponds to the second parameter of the `SpectralCMDHome` constructor.
+ *
+ * @returns A `CommandEntry` object representing the defined home command.
+ */
+export function defineHome(
+  options: ConstructorParameters<typeof SpectralCMDHome>[0],
+  config: ConstructorParameters<typeof SpectralCMDHome>[1]
+): CommandEntry;
+
+/**
+ * Defines a command entry for a home object.
+ *
+ * @param home - The home object to define the command entry for.
+ *               It can be either a `SpectralCMDHome` or a `ReduxCMDHome`.
+ * @returns A `CommandEntry` object representing the defined command entry.
+ */
+export function defineHome(home: SpectralCMDHome | ReduxCMDHome): CommandEntry;
+
+export function defineHome(
+  optionsOrHome:
+    | ConstructorParameters<typeof SpectralCMDHome>[0]
+    | SpectralCMDHome
+    | ReduxCMDHome,
+  configOrNot?: ConstructorParameters<typeof SpectralCMDHome>[1]
+): CommandEntry {
+  if (
+    (optionsOrHome instanceof SpectralCMDHome ||
+      optionsOrHome instanceof ReduxCMDHome) &&
+    !configOrNot
+  ) {
+    return (ctx) => optionsOrHome.runInContext(ctx);
+  } else if (
+    !(
+      optionsOrHome instanceof SpectralCMDHome ||
+      optionsOrHome instanceof ReduxCMDHome
+    )
+  ) {
+    const home = new SpectralCMDHome(optionsOrHome, configOrNot);
+    return (ctx) => home.runInContext(ctx);
+  }
 }
