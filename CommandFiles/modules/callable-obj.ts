0863d17bbbbcffb19f1178eeedbf3392aff05abd
lianecagara <nealiana.kaye.cagara@gmail.com>
2025-04-06 11:38:09 +0800

    feat(callable-obj): add Namespace constructor for enhanced callable object creation

--- CommandFiles/modules/callable-obj.ts
+++ CommandFiles/modules/callable-obj.ts
@@ -19,3 +19,21 @@ export function createNamespace<
   const from = Object.fromEntries(ownKeys.map((i) => [i, result[i]]));
   return Object.assign(safeTarget, from ?? ({} as R));
 }
+
+interface NamespaceConstructor {
+  new <T extends (...args: any[]) => any, M extends Record<string, any>>(
+    callback: (ns: T) => M,
+    target: T
+  ): Namespace<T, M>;
+}
+
+type Namespace<T, M> = ReturnType<typeof createNamespace<T, M>>;
+
+export const Namespace: NamespaceConstructor = class Namespace<
+  T extends (...args: any[]) => any,
+  M extends Record<string, any>
+> {
+  constructor(callback: (ns: T) => M, target: T) {
+    return createNamespace(callback, target);
+  }
+} as NamespaceConstructor;
