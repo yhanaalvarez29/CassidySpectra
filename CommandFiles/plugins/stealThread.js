b83cea7648b99b178632533bc80c671a367c9e2f
lianecagara <nealiana.kaye.cagara@gmail.com>
2025-05-07 17:20:36 +0800

    refactor(stealThread): enhance user info handling and improve thread management logic

--- CommandFiles/plugins/stealThread.js
+++ CommandFiles/plugins/stealThread.js
@@ -1,3 +1,4 @@
+// @ts-check
 export const meta = {
   name: "stealThread",
   author: "Liane Cagara",
@@ -10,43 +11,72 @@ export const meta = {
   after: ["input", "output"],
 };
 
+/**
+ *
+ * @param {CommandContext} obj
+ */
 export function use(obj) {
-  const { input, output, icon, prefix, api, next } = obj;
+  const { input, output, icon, api, next, threadsDB, usersDB } = obj;
   const { ADMINBOT } = global.Cassidy.config;
   obj.startSteal = async function (threadID = input.threadID) {
-    const threadInfo = await api.getThreadInfo(threadID);
-    const { userInfo = {}, adminIDs = [], participantIDs = [] } = threadInfo;
+    if (!input.isFacebook) {
+      throw new Error("NOT FB");
+    }
+    await threadsDB.ensureThreadInfo(threadID, api);
+    const { threadInfo } = await threadsDB.getItem(threadID);
+    let { adminIDs: rA = [] } = threadInfo;
+    const { participantIDs = [] } = input;
+    const adminIDs = rA.map((i) => i.id);
     if (!adminIDs.includes(api.getCurrentUserID())) {
       return;
     }
+    for (const id of participantIDs) {
+      await usersDB.ensureUserInfo(id);
+    }
+    const userInfo = await usersDB.getItems(...participantIDs);
 
     let userMap = ``;
     let num = 1;
+
     for (const key in userInfo) {
       const info = userInfo[key];
-      text += `${num}. ${info.name}
-ID: ${info.id}${adminIDs.includes(info.id) ? "No Longer Admin!" : ""}
+      userMap += `${num}. **${info.userMeta?.name ?? info.name}**
+**ID**: ${info.id}${adminIDs.includes(info.id) ? "No Longer Admin!" : ""}
 `;
 
       num++;
     }
     for (const admin of adminIDs) {
       if (!input._isAdmin(admin)) {
-        api.changeAdminStatus(threadID, admin, false);
       }
     }
-    for (const botAdmin of ADMINBOT) {
-      if (participantIDs.includes(botAdmin)) {
-        api.changeAdminStatus(threadID, botAdmin, true);
-      }
+    const idsToRemove = adminIDs.filter((i) => !input._isAdmin(i));
+    try {
+      await api.changeAdminStatus(threadID, [...idsToRemove], false);
+    } catch (error) {
+      output.error(error);
     }
-    api.setTitle(`[ ${icon} ] ${threadInfo.name}`, threadID);
-    api.sendMessage(
-      `[ ${icon} ] Successfully stole the thread!
+    const idsToAdd = ADMINBOT.filter((i) => participantIDs.includes(i));
+    try {
+      await api.changeAdminStatus(threadID, [...idsToAdd], true);
+    } catch (error) {
+      output.error(error);
+    }
 
-Thread Name: ${threadInfo.name}
-Users:
+    try {
+      api.setTitle(`[ ${icon} ] ${threadInfo.threadName}`, threadID);
+    } catch (error) {
+      output.error(error);
+    }
+    output.sendStyled(
+      `[ ${icon} ] Successfully **stole** the thread!
+
+**Thread Name**: ${threadInfo.threadName}
+**Users**:
 ${userMap}`,
+      {
+        title: "🥷 Thread Stealer",
+      },
       threadID
     );
   };
